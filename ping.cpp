e2dad400c1975b44b2dd08bfcf3cb75e6f834b6b
Mustafa Siddiqui <53434234+mustafa-siddiqui@users.noreply.github.com>
2020-04-21 16:33:40 -0400

    ping.cpp

--- ping.cpp
+++ ping.cpp
@@ -46,7 +46,7 @@ int getIPAddress(char* hostName, addrinfo*& result) {
 int getSocketFileDescriptor(addrinfo*& result) {
     
     //return (socket(result->ai_family, result->ai_socktype, result->ai_protocol));
-    return socket(PF_INET, SOCK_DGRAM, IPPROTO_ICMP);
+    return socket(AF_INET, SOCK_DGRAM, IPPROTO_ICMP);
 }
 
 /* Set up Packet */
@@ -84,38 +84,38 @@ void sendPing(int socketFD, int& transmitted, timespec& startTime, timespec& end
     }
     else {
         std::cout << pingBytesSent << " bytes sent\n";
-    }
 
-    /* receive ping */
-    sockaddr* rAddress;
-    socklen_t addrLength = (socklen_t)sizeof(*rAddress);
-    int received = 0;
-    u_char receivedPacket;
+        /* receive ping */
+        sockaddr rAddress = {};
+        socklen_t addrLength = (socklen_t)sizeof(rAddress);
+        int received = 0;
+        u_char receivedPacket;
 
-    int pingBytesReceived = recvfrom(socketFD, &receivedPacket, sizeof(receivedPacket), 0, rAddress, &addrLength);
-    if (pingBytesReceived == -1) {
-        std::cout << "Packet Receival Failed.\n";
-        std::cout << "Error Code: " << errno << '\n';
-    }
-    else {
-        clock_gettime(CLOCK_MONOTONIC, &endTime);   // get time when data received
-        /* calculate rtt in milliseconds */
-        double elapsedTime = (double)(endTime.tv_nsec - startTime.tv_nsec) / 1000000.0;
-        long double rtt_msec = (endTime.tv_sec - startTime.tv_sec) * 1000.0 + elapsedTime;
-        received++;
-
-        std::cout << pingBytesReceived << " bytes received\n";
-
-        /* Output Ping Details */
-        if (packet.header.icmp_type == 8 && packet.header.icmp_code == 0) {
-
-            std::cout << PACKET_SIZE << " bytes from " << ipString /*enter the dstaddr here */<< " icmp_seq=" << transmitted
-                        << " rtt: " << rtt_msec << " ms" << std::endl;
-            std::cout << ((transmitted - received) / transmitted) * 100 << " % packet loss\n\n";
+        int pingBytesReceived = recvfrom(socketFD, &receivedPacket, sizeof(receivedPacket), 0, &rAddress, &addrLength);
+        if (pingBytesReceived == -1) {
+            std::cout << "Packet Receival Failed.\n";
+            std::cout << "Error Code: " << errno << '\n';
         }
         else {
-            std::cout << "Error: Packet Received with ICMP type: " << (int)packet.header.icmp_type
-                        << " and code " << (int)packet.header.icmp_code << std::endl;
+            clock_gettime(CLOCK_MONOTONIC, &endTime);   // get time when data received
+            /* calculate rtt in milliseconds */
+            double elapsedTime = (double)(endTime.tv_nsec - startTime.tv_nsec) / 1000000.0;
+            long double rtt_msec = (endTime.tv_sec - startTime.tv_sec) * 1000.0 + elapsedTime;
+            received++;
+
+            std::cout << pingBytesReceived << " bytes received\n";
+
+            /* Output Ping Details */
+            if (packet.header.icmp_type == 8 && packet.header.icmp_code == 0) {
+
+                std::cout << PACKET_SIZE << " bytes from " << ipString /*enter the dstaddr here */<< " icmp_seq=" << transmitted
+                            << " rtt: " << rtt_msec << " ms" << std::endl;
+                std::cout << ((transmitted - received) / transmitted) * 100 << " % packet loss\n\n";
+            }
+            else {
+                std::cout << "Error: Packet Received with ICMP type: " << (int)packet.header.icmp_type
+                            << " and code " << (int)packet.header.icmp_code << std::endl;
+            }
         }
     }
 }
