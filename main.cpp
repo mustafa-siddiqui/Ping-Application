0134ce82978a455c68b7d86d8f18d2316c05fb4e
Mustafa Siddiqui <53434234+mustafa-siddiqui@users.noreply.github.com>
2020-04-21 00:10:56 -0400

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,23 @@
-/** @file main.cpp
- *  @brief 
+/** @file   main.cpp
+ *  @brief  C++ Implementation for a Ping Program.
+ *          Uses ICMP and Datagram Sockets.
+ *          Compile: g++ main.cpp ping.cpp -o ping
+ *          Run: ping <domain_name / IP>
  *  @author Mustafa Siddiqui
- *  @date 4/17/20
+ *  @date   4/20/20
  */
 
 #include <iostream>
 #include <string>
-#include "ping.hpp"
 #include <arpa/inet.h>  // inet_ntop()
 #include <netinet/in.h>
+// -- //
+#include "ping.hpp"
 
+/** @fn     main(int, char**)
+ *  @brief  Main function of the program. Uses functions from ping.hpp to
+ *          implement 'ping'.
+ */
 int main(int argc, char* argv[]) {
 
     /* validate arguments */
@@ -19,6 +27,7 @@ int main(int argc, char* argv[]) {
     }
 
     addrinfo* result;
+    sockaddr_in* pingAddress;
     char ipString[INET_ADDRSTRLEN];                                         // character array to hold ip address as a string
     int ipStatus = getIPAddress(argv[1], result);
     if (ipStatus != 0) {                                                    // if some error (result is passed by reference here)
@@ -27,11 +36,11 @@ int main(int argc, char* argv[]) {
     }
     else {
         
-        sockaddr_in* ipv4 = (sockaddr_in*)result->ai_addr;                  // get struct from resulting link list
+        pingAddress = (sockaddr_in*)result->ai_addr;                        // get struct from resulting link list
         void* address;
-        address = &ipv4->sin_addr;                                          // store ip address
+        address = &pingAddress->sin_addr;                                   // store ip address
         inet_ntop(result->ai_family, address, ipString, sizeof(ipString));  // convert binary IP to string
-        std::cout << ipString << std::endl;
+        std::cout << "IP: " <<  ipString << std::endl;
     }
 
     int socketFD = getSocketFileDescriptor(result);
@@ -39,14 +48,15 @@ int main(int argc, char* argv[]) {
         std::cout << "Socket File Descriptor Not Received\n";
         return 1;
     }
+    /*
     else {
         std::cout << "Socket File Descriptor Received: " << socketFD << std::endl;
     }
+    */
 
     int transmitted = 0;
     timespec startTime, endTime;
     timeval tvOut = {RECV_TIMEOUT, 0};      // initialize struct
-    sockaddr_in* pingAddress = (sockaddr_in*)result->ai_addr;
 
     /* set socket option to TTL */
     int ttl_value = 64;
@@ -55,27 +65,33 @@ int main(int argc, char* argv[]) {
         std::cout << "Setting Socket Options Failed.\n";
         exit(EXIT_FAILURE);
     }
+    /*
     else {  // for debugging purposes
         std::cout << "Socket Option Set to TTL!\n";
     }
+    */
+
     /* set timeout of receival */
-    /*if (setsockopt(socketFD, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tvOut, sizeof(tvOut)) != 0) {
+    if (setsockopt(socketFD, SOL_SOCKET, SO_RCVTIMEO, (const char*)&tvOut, sizeof(tvOut)) != 0) {
         std::cout << "Setting Timeout for Receival Failed.\n";
         exit(EXIT_FAILURE);
     }
+    /*
     else {  // for debugging purposes
         std::cout << "Time of RECV set\n";
-    }*/
+    }
+    */
 
-    free(result);       // free linked list before infinite loop
+    //free(result);       
+    freeaddrinfo(result);   // free linked list before infinite loop
 
-    for (;;) {          // ping in infinite loop
+    for (;;) {              // ping in infinite loop
 
         sendPing(socketFD, transmitted, startTime, endTime, pingAddress, ipString);
         std::cout << std::endl;
     }
 
-    close(socketFD);    // close socket        
+    close(socketFD);        // close socket        
 
     return 0;
 }
